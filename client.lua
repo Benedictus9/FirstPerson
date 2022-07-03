bdf8ccbaebf5353986b8f2a7ab9314f0b2498c10
Benedictus9 <108435017+Benedictus9@users.noreply.github.com>
2022-07-03 18:33:07 +0200

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,34 @@
+--[[------------------------------------------------------------------------
+    First Person Only (Server Sided) Resource created by Daniel
+------------------------------------------------------------------------]]--
+local forceFirstPerson = true
+local cam = CreateCam("firstperson", true)
+local playerIdx = GetPlayerFromServerId(Citizen.Player)
+local ped = GetPlayerPed(-1)
+
+-- CODE --
+Citizen.CreateThread(function()
+    SetBlackout(false)
+		CreateCam("firstperson", true)
+
+    		while true do
+        		Wait(1)
+
+    			if forceFirstPerson then
+        		AttachCamToPedBone("firstperson", -1, 0x796E, 0, 0, 0.15, 0.5, true)
+
+			end
+		end
+	end
+end)
+
+-- ATTACH_CAM_TO_PED_BONE
+--AttachCamToPedBone(
+--	cam --[[ Cam ]], 
+--	ped --[[ Ped ]], 
+--	boneIndex --[[ integer ]], 
+--	x --[[ number ]], 
+--	y --[[ number ]], 
+--	z --[[ number ]], 
+--	heading --[[ boolean ]]
+--)
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'An improved version of the 1st person perspective for roleplay purposes.'
+version '1.0.0'
+
+client_script 'client.lua'
+
+lua54 'yes'
+
+author 'TheLukasGran, edited by Benedictus9'
